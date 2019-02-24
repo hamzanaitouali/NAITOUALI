20d5efb9c6e35dd8bb90f6631c7ce2c3cbf715c5
hamzanaitouali <47951570+hamzanaitouali@users.noreply.github.com>
2019-02-24 20:50:46 +0100

    Update CCC '16 J1 - Tournament Selection.c

--- CCC '16 J1 - Tournament Selection.c
+++ CCC '16 J1 - Tournament Selection.c
@@ -1,6 +1,6 @@
 //# NAITOUALI
 //PROG
-*/Each player in a tournament plays six games. There are no ties. The tournament director places the players in groups based on the results of games as follows:
+/*Each player in a tournament plays six games. There are no ties. The tournament director places the players in groups based on the results of games as follows:
 
 if a player wins 5 or 6 games, they are placed in Group 1;
 if a player wins 3 or 4 games, they are placed in Group 2;
@@ -13,6 +13,7 @@ The input consists of six lines, each with one of two possible letters: W (to in
 
 Output Specification
 The output will be either 1, 2, 3 (to indicate which Group the player should be placed in) or -1 (to indicate the player has been eliminated).
+-------------------------------------------------------------------------------------------------------------------------------------------
 */
 #include <stdio.h>
 #include <string.h>
